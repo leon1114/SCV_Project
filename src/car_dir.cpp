91ada6fa8ac32ddd6eb18bbd7cd943ebb96801d6
hanchanhwnag <lintz75@gmail.com>
2017-06-07 11:13:37 +0900

    Add car_dir driver

---
+++ src/car_dir.cpp
@@ -0,0 +1,64 @@
+
+#include <wiringPi.h>
+#include <stdio.h>
+#include "car_dir.h"
+
+
+int leftPWM, rightPWM, homePWM;
+PCA9685 pwm;
+
+int Map(int x, int in_min, int in_max, int out_min, int out_max)
+{
+	return (x-in_min) * (out_max-out_min) / (in_max-in_min) + out_min;
+}
+
+void setup(int bus, int address)
+{
+	int offset = 0;
+	leftPWM = 400, homePWM = 450, rightPWM = 500;
+	leftPWM += offset;
+	homePWM += offset;
+	rightPWM += offset;
+
+	pwm = new PCA9685(bus, address);
+	pwm.setPWMFreq(60);
+}
+
+void turn_left()
+{
+	pwm.setPWM(CH0,0,leftPWM);
+}
+
+void turn_right()
+{
+	pwm.setPWM(CH0,0,rightPWM);
+}
+
+void turn(int angle)
+{
+	angle = Map(angle, 0, 255, leftPWM, rightPWM);
+	pwm.setPWM(CH0,0,angle);
+}
+
+void home()
+{
+	pwm.setPWM(CH0,0,homePWM);
+}
+
+void calibrate(int dx)
+{
+	pwm.setPWM(CH0,0,450+dx);
+}
+
+void test()
+{
+	while(true){
+		turn_left();
+		delay(1);
+		home();
+		delay(1);
+		turn_right();
+		delay(1);
+		home();
+	}
+}
---
+++ src/car_dir.h
@@ -0,0 +1,15 @@
+#ifndef _CAR_DIR_H
+#define _CAR_DIR_H
+
+#define CH0 0
+
+int Map(int x, int in_min, int in_max, int out_min, int out_max);
+void setup(int bus, int address);
+void turn_left();
+void turn_right();
+void turn(int angle);
+void home();
+void calibrate(int dx);
+void test();
+
+#endif
