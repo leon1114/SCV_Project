fdb567130a3d0de1be88c186756a495efa116f64
leon1114 <leon1114@naver.com>
2017-06-08 15:46:45 +0900

    lane keeping modified

---
+++ src/lanekeeping.cpp
@@ -0,0 +1,81 @@
+#include "lanekeeping.h"
+#include "camera.h"
+#include "motor.h"
+#include "car_dir.h"
+
+using namespace std;
+using namespace cv;
+
+Mat img;
+
+// setting variables
+Point pt;
+Mat gray_img;
+
+Scalar left_val;
+Scalar right_val;
+int left_lane_dist = 0;
+int right_lane_dist = 0;
+int road_ended = 0;
+
+void laneKeepingControl()
+{
+	pt.x = 327;
+	pt.y = 350;
+    int left_lane_dist = 0;
+    int right_lane_dist = 0;
+    volatile int i;
+	img = getFrame();
+    cvtColor(img, gray_img, COLOR_BGR2GRAY);
+    inRange(gray_img,180,255,gray_img);
+
+    for (i=1;i<350;i++)
+     {
+         left_val = gray_img.at<uchar>(pt.y, pt.x - i);
+         right_val = gray_img.at<uchar>(pt.y, pt.x + i);
+         if (!left_lane_dist &&left_val.val[0] != 0)
+         {
+             left_lane_dist = i;
+         }
+         if (!right_lane_dist && right_val.val[0] != 0)
+         {
+             right_lane_dist = i;
+         }
+     }
+
+    printf("Dist to left lane : %d, Dist to right lane : %d\n", left_lane_dist, right_lane_dist);
+
+    if (left_lane_dist && right_lane_dist)
+    {
+    	home();
+    	setSpeed(BASIC_SPEED);
+    }
+    else if (left_lane_dist)
+    {
+    	turnRight();
+    	setSpeed(BASIC_SPEED);
+    }
+    else if (right_lane_dist)
+    {
+    	turnLeft();
+    	setSpeed(BASIC_SPEED);
+    }
+    else
+    {
+    	road_ended = 1;
+    	motorTerm();
+    	dirTerm();
+    	printf("ended\n");
+    }
+
+
+    if (left_lane_dist) circle(img, Point(pt.x - left_lane_dist, 350), 10, Scalar(0,0,255),-1,8);
+    if (right_lane_dist) circle(img, Point(pt.x + right_lane_dist, 350), 10, Scalar(0,0,255),-1,8);
+
+	imshow("test", img);
+	if (road_ended)
+	{
+		imshow("test", gray_img);
+		waitKey(0);
+	}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,14 @@
 #include "motor.h"
 #include "car_dir.h"
 #include "camera.h"
+#include "lanekeeping.h"
 
 using namespace cv;
 using namespace std;
-
-Mat img;
-
-// setting variables
-Point pt;
-Mat gray_img;
-
-Scalar left_val;
-Scalar right_val;
-int left_lane_dist = 0;
-int right_lane_dist = 0;
-int road_ended = 0;
-
+#define BASIC_SPEED 50
 
 void Init()
 {
-	pt.x = 327;
-	pt.y = 350;
-
 	cameraInit();
 	motorInit();
 	dirInit();
@@ -45,78 +31,18 @@ volatile int usflag;
 
 int main(int argc, char *argv[])
 {
-
 	int ret;
 	pthread_t threadId;
 	const char *msgThread = "Ultrasonic detection thread";
 	void *threadRet;
 
-
 	Init();
 	forward();
-	// ========================================
-	printf("lane test INIT\n");
 
 	//ret = pthread_create(&threadId, NULL, US, (void *)msgThread);
 	while(1)
 	{
-
-        int left_lane_dist = 0;
-        int right_lane_dist = 0;
-        volatile int i;
-		img = getFrame();
-        cvtColor(img, gray_img, COLOR_BGR2GRAY);
-        inRange(gray_img,200,255,gray_img);
-
-        for (i=1;i<327;i++)
-         {
-             left_val = gray_img.at<uchar>(pt.y, pt.x - i);
-             right_val = gray_img.at<uchar>(pt.y, pt.x + i);
-             if (!left_lane_dist &&left_val.val[0] != 0)
-             {
-                 left_lane_dist = i;
-             }
-             if (!right_lane_dist && right_val.val[0] != 0)
-             {
-                 right_lane_dist = i;
-             }
-         }
-
-
-        if (left_lane_dist && right_lane_dist)
-        {
-        	printf("forward!!");
-        	home();
-        	setSpeed(25);
-        }
-        else if (left_lane_dist)
-        {
-        	turnRight();
-        	setSpeed(25);
-        }
-        else if (right_lane_dist)
-        {
-        	turnLeft();
-        	setSpeed(25);
-        }
-        else
-        {
-        	road_ended = 1;
-        	printf("ended\n");
-        }
-
-        if(road_ended) break;
-
-        printf("Dist to left lane : %d, Dist to right lane : %d\n", left_lane_dist, right_lane_dist);
-        if (left_lane_dist) circle(img, Point(pt.x - left_lane_dist, 350), 10, Scalar(0,0,255),-1,8);
-        if (right_lane_dist) circle(img, Point(pt.x + right_lane_dist, 350), 10, Scalar(0,0,255),-1,8);
-
-		imshow("test", img);
-
-		/******** CV analysis start ********/
-
-
-		/******** CV analysis finish ********/
+		laneKeepingControl();
 
 //		if (usflag == 1){
 //			int prevSpeed = getSpeed();
@@ -124,18 +50,9 @@ int main(int argc, char *argv[])
 //			while(usflag);
 //			forwardWithSpeed(prevSpeed);
 //		}
-
-		/******** Car control start********/
-
-
-		/******** Car control finish ********/
-
 		if(cv::waitKey(20) == 27) break;
 	}
 
-
-
-	Terminate();
     return 0;
 }
 
